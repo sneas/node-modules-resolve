416076f22d8aaae6358c7c7e09f84ac2699d85a2
sergey-tom <sergey-tom@users.noreply.github.com>
2016-11-14 12:55:19 +0100

    Add "root" parameter to getPath function.
    Add "root" parameter in order to fix never-ending recursion on Windows.

--- src/index.js
+++ src/index.js
@@ -2,21 +2,22 @@ var stack = require('callsite'),
     path = require('path'),
     fs = require('fs');
 
-function getPath(nmPath, workingDir) {
+function getPath(nmPath, workingDir, root) {
     var pathToCheck = path.resolve(workingDir, 'node_modules', nmPath);
 
     if (fs.existsSync(pathToCheck)) {
         return pathToCheck;
     }
 
-    if (workingDir ==='/') {
-        throw new Error('Unable to resolve "' + nmPath +'"');
+    if (workingDir === root) {
+        throw new Error('Unable to resolve "' + nmPath + '"');
     }
 
-    return getPath(nmPath, path.dirname(workingDir));
+    return getPath(nmPath, path.dirname(workingDir), root);
 }
 
 module.exports = function(nmPath) {
     var initialPath = path.dirname(stack()[1].getFileName());
-    return getPath(nmPath, initialPath);
+    var root = path.parse(initialPath).root;
+    return getPath(nmPath, initialPath, root);
 };
